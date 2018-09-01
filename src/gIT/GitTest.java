c975e94dce2a8a485dcfe4d7e9f60d95cfc759a8
Roman <romansd@bk.ru>
2018-09-01 21:19:41 +0300

    изменения

--- src/gIT/GitTest.java
+++ src/gIT/GitTest.java
@@ -3,10 +3,11 @@ package gIT;
 public class GitTest {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Привет как дела?");
+		int i = 150;
+		int b = 13;
+		int c = 12;
 		System.out.println("Hello world");
-		
-		System.out.println(" vsem privet ");
 	}
 
 }
